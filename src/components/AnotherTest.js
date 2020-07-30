ea7bafe4abd2bb2aab0f2706f4c8ed2aee8436ff
Joseph <jtawil2015@gmail.com>
2020-07-29 22:15:13 -0800

    Learning react app practice

---
+++ src/components/AnotherTest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const AnotherTest = () => {
+
+return(
+    <div>
+       <h1>Hewoooooo</h1> 
+    </div>
+);
+}
---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Test = () => {
+    return (
+        <div>
+            <h1> No Way</h1>
+        </div>
+    )
+}
