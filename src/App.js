3af1ac080a2bf585ec3129062bd5234947cb6548
Joseph <jtawil2015@gmail.com>
2020-08-05 21:34:57 -0800

    created a mathy operator function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, {useState} from 'react';
-import Button from './components/Button';
-import './App.css';
-
+// import Button from './components/Button';
+// import './App.css';
+import Mathy from './components/Mathy/Mathy';
 function App() {
 
-  const [counter, modifyCounter] = useState(0);
+  // const [counter, modifyCounter] = useState(0);
 
   return (
     <div className="App">
-      {counter}
-      
-     <Button float= {true} size={"large"} waveEffect={true} waveColor="light" color= "blue"><i onClick = {()=>{
-       console.log("hello")
-       let tempCounter = counter;
-       tempCounter++;
-       modifyCounter(tempCounter);
-       console.log(tempCounter);
-       } }className="material-icons">add</i></Button>
-     <Button float= {false} size={"small"} waveEffect={false} waveColor="dark" color= "red"><i className="material-icons">cloud</i></Button>
+      <Mathy num1={1} operator = {"multiply"} num2={2}></Mathy>
     </div>
   );
 }
 
 export default App;
+
+
+// {counter}
+      
+//      <Button float= {true} size={"large"} waveEffect={true} waveColor="light" color= "blue"><i onClick = {()=>{
+//        console.log("hello")
+//        let tempCounter = counter;
+//        tempCounter++;
+//        modifyCounter(tempCounter);
+//        console.log(tempCounter);
+//        } }className="material-icons">add</i></Button>
+//      <Button float= {false} size={"small"} waveEffect={false} waveColor="dark" color= "red"><i className="material-icons">cloud</i></Button>
---
+++ src/components/Mathy/Mathy.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Mathy = (props) => {
+    let result;
+
+    switch (props.operator) {
+        case "plus":
+            result = props.num1 + props.num2;
+            break;
+        
+        case "minus":
+            result = props.num1 - props.num2;
+            break;
+    
+        case "divide":
+            result = props.num1 / props.num2;
+            break;
+
+        case "multiply":
+            result = props.num1 * props.num2;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return(
+        <h1>{result}</h1>    
+    );
+};
+
+
+Mathy.propTypes = {
+    num1: PropTypes.number.isRequired,
+    num2: PropTypes.number.isRequired,
+    operator: PropTypes.string.isRequired,
+}
+export default Mathy;
